e2f159496c033b37319ee6868c3807456ab31362
DalKinzel <dkinzel@gmail.com>
2013-02-26 16:16:11 -0700

    initial commit

---
+++ www/main.js
@@ -0,0 +1,35 @@
+﻿var game = {
+    InitialWidth: 1280,
+    InitialHeight: 800,
+    Width: window.innerWidth,
+    Height: window.innerHeight
+}
+
+$(window).resize(function () {
+    game.Width = window.innerWidth;
+    game.Height = window.innerHeight;
+    
+    resize(game.Width, game.Height);
+});
+
+$(document).ready(function () {
+    
+});
+
+
+function resize(width, height) {
+    //$("#gameArea").width = width;
+    //$("#gameArea").height = height;
+
+    //$("#gameArea").prop({
+    //    width: width,
+    //    height: height
+    //});
+
+    //$("#gameCanvas")[0].width = width;
+    //$("#gameCanvas")[0].height = height;
+
+    game.ScaleFactorX = game.Width / game.InitialWidth;
+    game.ScaleFactorY = game.Height / game.InitialHeight;
+}
+
