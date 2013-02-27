32417fd1579e544cf2cc35ba2ecc2b392e247c72
DalKinzel <dkinzel@gmail.com>
2013-02-27 16:24:56 -0700

    added individual divs for each svg object

--- www/main.js
+++ www/main.js
@@ -6,22 +6,10 @@
 }
 
 $(window).resize(function () {
-    var lion = $("#lion")[0];
-    lion.setAttribute("transform", "scale(2)")
-    lion.transform.baseVal.getItem(0).setScale(5,5);
-    
-
-    //$("#elephant")
-    //$("#giraffe")
-    //$("#ibex")
-    //$("#lion")
-    //$("#lioncub")
-    //$("#rhino")
-
     game.Width = window.innerWidth;
     game.Height = window.innerHeight;
     
-    //resize(game.Width, game.Height);
+    resize(game.Width, game.Height);
 });
 
 $(document).ready(function () {
@@ -29,22 +17,48 @@ $(document).ready(function () {
     $("body").css("background-image", "url(resources/images/svg/backgrounds/house-with-snow.svg)");
     $("body").css("background-repeat", "no-repeat");
     $("body").css("background-size", "cover");
+
+    resize(game.Width, game.Height);
+
+    $("#svgElephant").svg({ onLoad: null });
+    $("#svgLion").svg({ onLoad: null });
+    $("#svgGiraffe").svg({ onLoad: null });
+    $("#svgIbex").svg({ onLoad: null });
+    $("#svgLion").svg({ onLoad: null });
+    $("#svgLionCub").svg({ onLoad: null });
+    $("#svgRhino").svg({ onLoad: loadResources });
 });
 
 
+function loadResources() {
+    $("#svgRhino").svg('get').load("resources/images/svg/african-animals/rhino.svg", { addTo: false, changeSize: true, onLoad: null });
+    $("#svgLionCub").svg('get').load("resources/images/svg/african-animals/lioncub.svg", { addTo: false, changeSize: true, onLoad: null });
+    $("#svgLion").svg('get').load("resources/images/svg/african-animals/lion.svg", { addTo: false, changeSize: true, onLoad: null });
+    $("#svgIbex").svg('get').load("resources/images/svg/african-animals/ibex.svg", { addTo: false, changeSize: true, onLoad: null });
+    $("#svgGiraffe").svg('get').load("resources/images/svg/african-animals/giraffe.svg", { addTo: false, changeSize: true, onLoad: null });
+    $("#svgElephant").svg('get').load("resources/images/svg/african-animals/elephant.svg", {addTo: false,changeSize: true,onLoad: null});
+}
+
 function resize(width, height) {
-    //$("#gameArea").width = width;
-    //$("#gameArea").height = height;
+    var svg = $("#svgRhino").svg('get');
+    if (svg) {
+        $("#svgElephant").css("width", "100px");
+        $("#svgElephant").css("height", "100px");
+        $("#svgLion").css("width", "100px");
+        $("#svgLion").css("height", "100px");
+        $("#svgGiraffe").css("width", "100px");
+        $("#svgGiraffe").css("height", "100px");
+        $("#svgIbex").css("width", "100px");
+        $("#svgIbex").css("height", "100px");
+        $("#svgLion").css("width", "100px");
+        $("#svgLion").css("height", "100px");
+        $("#svgLionCub").css("width", "100px");
+        $("#svgLionCub").css("height", "100px");
+        $("#svgRhino").css("width", "100px");
+        $("#svgRhino").css("height", "100px");
 
-    //$("#gameArea").prop({
-    //    width: width,
-    //    height: height
-    //});
-
-    //$("#gameCanvas")[0].width = width;
-    //$("#gameCanvas")[0].height = height;
-
-    game.ScaleFactorX = game.Width / game.InitialWidth;
-    game.ScaleFactorY = game.Height / game.InitialHeight;
+        game.ScaleFactorX = game.Width / game.InitialWidth;
+        game.ScaleFactorY = game.Height / game.InitialHeight;
+    }
 }
 
